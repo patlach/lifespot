aab4bbb2ade26b48ef9edcf1c83597dafe9c102d
Egor <>
2023-03-29 10:48:35 +0300

    Add module 26 27 28

---
+++ LifeSpot/Static/JS/index.js
@@ -0,0 +1,18 @@
+﻿let map = new Map();
+
+map.set('UserData', window.navigator.userAgent);
+map.set('DateTime', new Date());
+map.set('Age', prompt('Enter your age'));
+
+if (map.get('Age') < 18) {
+    alert("Only over 18 years of age can use this site. You will redirect to google.com.");
+    window.location.href = "http://www.google.com";
+}
+else {
+    alert('Welcome to LifeSpot');
+    map.set('DateTime', new Date());
+}
+
+for (let element of map) {
+    console.log(element);
+}
---
+++ LifeSpot/Static/JS/testing.js
@@ -0,0 +1,8 @@
+﻿
+let elements = document.getElementsByTagName('*');
+let arr = [1, 2, 3]
+
+arr.forEach(function (item, index, array) {
+    console.log(item);
+});
+alert(`Количество элементов на странице:  ${elements.length}`);
